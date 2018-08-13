d13b078621d7c405786d798d382320ee98c14c36
Kislov Dmitry <kislov@easydan.com>
2018-08-13 12:51:01 +1000

    show random images on startup

--- rhdb/main/processors.py
+++ rhdb/main/processors.py
@@ -1,6 +1,7 @@
-from .models import Page
+from .models import Page, Image
 
 
 def base(request):
     return {'pages': Page.objects.filter(public=True),
+            'random_images': Image.objects.filter(public=True)
             }
