bb2f5f51ab10e4dbddea17e8393fdec0ec056294
Kislov Dmitry <kislov@easydan.com>
2018-08-13 14:02:50 +1000

    total records processor added

--- rhdb/main/processors.py
+++ rhdb/main/processors.py
@@ -1,7 +1,8 @@
-from .models import Page, Image
+from .models import Page, Image, Record
 
 
 def base(request):
     return {'pages': Page.objects.filter(public=True),
-            'random_images': Image.objects.filter(public=True)
+            'random_images': Image.objects.filter(public=True),
+            'total': Record.objects.all().count()
             }
